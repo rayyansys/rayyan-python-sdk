4e869b41535599f8f66c717e0e7f8d2cd8f51a1a
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-06-26 20:57:46 +0300

    add paths file

---
+++ rayyan/paths.py
@@ -0,0 +1,10 @@
+BASE_URL = "https://staging.rayyan.ai"
+REFRESH_TOKEN_ROUTE = "/oauth/token"
+# ________user________
+DELETE_USER_ROUTE = "/api/v1/users"
+USER_INFO_ROUTE = "/api/v1/user_info"
+REVOKE_TOKEN_ROUTE = "/oauth/revoke"
+# ________review________
+REVIEWS_ROUTE = "/api/v1/reviews"
+# ________review________
+MENDELEY_ROUTE = "/api/v1/third_party_auth"
