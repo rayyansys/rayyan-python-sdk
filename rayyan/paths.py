a739a3e9a1dd74a58da4c6162623b0f3eea0ccd3
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-07-18 14:07:15 +0300

    add user paths

--- rayyan/paths.py
+++ rayyan/paths.py
@@ -1,3 +1,7 @@
 # ________base________
 BASE_URL = "https://staging.rayyan.ai"
 REFRESH_TOKEN_ROUTE = "/oauth/token"
+# ________user________
+DELETE_USER_ROUTE = "/api/v1/users"
+USER_INFO_ROUTE = "/api/v1/user_info"
+REVOKE_TOKEN_ROUTE = "/oauth/revoke"
