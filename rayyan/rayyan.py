a416566d87229bbd871ce3db63ee59aa95c4f75e
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-07-24 18:48:59 +0300

    remove credentials handler from rayyan main class

--- rayyan/rayyan.py
+++ rayyan/rayyan.py
@@ -7,12 +7,5 @@ class Rayyan:
     def __init__(self, credentials_file_path: str):
         self.user = User(self)
         self._request = Request(self)
-        self._credentials_file_path = credentials_file_path
         self._base_url = BASE_URL
-        self._credentials_handler()
-
-    def _credentials_handler(self) -> None:
-        _credentials = self._request.get_credentials_from_credentials_file()
-        self._access_token = _credentials["access_token"]
-        self._refresh_token = _credentials["refresh_token"]
-        self._token_type = _credentials["token_type"]
+        self._request.credentials_file_handler(credentials_file_path)
