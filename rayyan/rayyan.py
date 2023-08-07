97849dc30b3c47b87d08c1b185fb020d8d74e823
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-08-07 09:11:10 +0300

    add review access to main class

--- rayyan/rayyan.py
+++ rayyan/rayyan.py
@@ -1,11 +1,13 @@
 from rayyan.request import Request
 from rayyan.user import User
+from rayyan.review_access import ReviewAccess
 from rayyan.paths import BASE_URL
 
 
 class Rayyan:
     def __init__(self, credentials_file_path: str, url: str = BASE_URL):
         self.user = User(self)
+        self.review_access = ReviewAccess(self)
         self.request = Request(self)
         self._base_url = url
         self.request._credentials_file_handler(credentials_file_path)
