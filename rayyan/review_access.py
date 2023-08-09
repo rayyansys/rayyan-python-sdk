dea3f075921386283782c3aa4564297a2c3b86d7
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-08-09 12:46:07 +0300

    add type hints to forgotten variables

--- rayyan/review_access.py
+++ rayyan/review_access.py
@@ -13,14 +13,14 @@ class ReviewAccess:
     def __init__(self, rayyan: Rayyan):
         self.__rayyan__ = rayyan
 
-    def delete_access(self, id, user_id: int) -> dict:
+    def delete_access(self, id: int, user_id: int) -> dict:
         return self.__rayyan__.request.request_handler(
             method="POST",
             path=f"{REVIEWS_ROUTE}/{id}/delete_review_access",
             params=user_id,
         )
 
-    def update_access(self, id, role_id: int, user_emails: list) -> dict:
+    def update_access(self, id: int, role_id: int, user_emails: list) -> dict:
         return self.__rayyan__.request.request_handler(
             method="POST",
             path=f"{REVIEWS_ROUTE}/{id}/update_review_access",
