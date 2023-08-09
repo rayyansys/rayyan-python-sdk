bd5b1a66e036310031a07da3a68e8bdb3e0264a6
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-08-09 12:28:30 +0300

    update methods params id -> review_id

--- rayyan/notes.py
+++ rayyan/notes.py
@@ -13,22 +13,24 @@ class Notes:
     def __init__(self, rayyan: Rayyan):
         self.__rayyan__ = rayyan
 
-    def create_note(self, id, article_id: int, text: str) -> dict:
+    def create_note(self, review_id: int, article_id: int, text: str) -> dict:
         return self.__rayyan__.request.request_handler(
             method="POST",
-            path=f"{REVIEWS_ROUTE}/{id}/articles/{article_id}/notes",
+            path=f"{REVIEWS_ROUTE}/{review_id}/articles/{article_id}/notes",
             payload={"text": text},
         )
 
-    def update_note(self, id, article_id: int, note_id: int, text: str) -> dict:
+    def update_note(
+        self, review_id: int, article_id: int, note_id: int, text: str
+    ) -> dict:
         return self.__rayyan__.request.request_handler(
             method="PUT",
-            path=f"{REVIEWS_ROUTE}/{id}/articles/{article_id}/notes/{note_id}",
+            path=f"{REVIEWS_ROUTE}/{review_id}/articles/{article_id}/notes/{note_id}",
             payload={"text": text},
         )
 
-    def delete_note(self, id, article_id: int, note_id: int) -> dict:
+    def delete_note(self, review_id: int, article_id: int, note_id: int) -> dict:
         return self.__rayyan__.request.request_handler(
             method="DELETE",
-            path=f"{REVIEWS_ROUTE}/{id}/articles/{article_id}/notes/{note_id}",
+            path=f"{REVIEWS_ROUTE}/{review_id}/articles/{article_id}/notes/{note_id}",
         )
