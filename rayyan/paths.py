713467f1440a3e8e0229f511097020426a293c24
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-08-22 16:47:03 +0300

    add review path

--- rayyan/paths.py
+++ rayyan/paths.py
@@ -5,3 +5,5 @@ REFRESH_TOKEN_ROUTE = "/oauth/token"
 DELETE_USER_ROUTE = "/api/v1/users"
 USER_INFO_ROUTE = "/api/v1/user_info"
 REVOKE_TOKEN_ROUTE = "/oauth/revoke"
+# ________review________
+REVIEWS_ROUTE = "/api/v1/reviews"
